66773ec8807cfeb715813aab09c4a68840642f2b
Daniel Da Silva <dasilva505@gmail.com>
2020-08-19 19:15:29 +0200

    fix stat

--- lib/stat.php
+++ lib/stat.php
@@ -3,9 +3,10 @@ include 'lib/db.php';
 
     $ip   = $_SERVER['REMOTE_ADDR'];
 
-    $sql="INSERT INTO  stat(ip, date) VALUES(:ip,NOW())";
+    $sql="INSERT INTO  stat(ip, date) VALUES(:ip, :date)";
     $query = $db->prepare($sql);
     $query->bindParam(':ip',$ip);
+    $query->bindParam(':date',date("Y-m-d H:i:s"));
     $query->execute();
 
 ?> 
